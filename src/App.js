7bdb1e0223dacf1b17fbedae509d1a7bfa30e338
Dario Peroza <rudapesot@gmail.com>
2022-09-09 00:01:09 -0400

    Version 0.1

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import './App.css';
 
 function showImage(url) {
   if (url === '') {
-    return <img alt='Texto del año' src={'media/DefaultBackground.jpg'}/>
+    return <img id="mainMedia" alt='Texto del año' src={'media/DefaultBackground.jpg'}/>
   } else {
-    return <img alt='Imagen proyectada' src={url}/>
+    return <img id="mainMedia" alt='Imagen proyectada' src={url}/>
   }
 }
 
-function showVideo(file) {
-  console.log(file)
+function showVideo(url, playing = false, muted = false) {
+  return <ReactPlayer
+    id="mainMedia"
+    playing={playing}
+    muted={muted}
+    url={url}
+    width={'100%'}
+    height={'100%'}
+    volume={1}
+  />
 }
 
 function showVideoThumbnail(url) {
   return <ReactPlayer
     light={true}
-    previewTabIndex={75}
     url={url}
     width={'100%'}
     height={'100%'}
-    controls={false}
   />
 }
 
@@ -30,18 +36,80 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      mediaPreview: {},
       activeFile: '',
       fileFormat: 'image',
-      mediaFiles: []
+      mediaFiles: [],
+      play: false,
+      mute: false
     }
   }
   componentDidMount() {
+    document.addEventListener('keydown', (event) => {
+      const keyName = event.key;
+      if (keyName === 'x') {
+        this.resetActiveFile()
+      }
+      if (keyName === 's') {
+        this.showMedia()
+      }
+      if (keyName === ' ' || keyName === 'p') {
+        this.playVideo()
+      }
+    });
     setInterval(e => {
       const videoThumbnails = document.querySelectorAll('.react-player__preview')
       if (videoThumbnails.length > 0) {
         videoThumbnails[0].click()
       }
-    }, 2000)
+    }, 1000)
+  }
+  mediaPreview(media, e) {
+    if (e.isTrusted) {    
+      const activeButton = document.querySelector('.activeMediaButton')
+      if (activeButton) {
+        activeButton.className = e.target.className
+      }
+      e.target.parentNode.className = 'activeMediaButton'
+      this.setState({mediaPreview: media})
+    }
+  }
+  resetActiveFile() {
+    this.setState({
+      activeFile: '', 
+      fileFormat: 'image',
+      playing: false,
+      mute: false
+    })
+  }
+  playVideo() {
+    this.setState({playing: !this.state.playing})
+  }
+  fullScreen() {
+    document.querySelector('#mainMedia').webkitRequestFullscreen()
+  }
+  showMedia() {
+    this.resetActiveFile()
+    if (Object.keys(this.state.mediaPreview).length > 0) {
+      const {url, type} = this.state.mediaPreview
+      this.setState({activeFile: url, fileFormat: type})
+    }
+  }
+  muteVideo() {
+    this.setState({mute: !this.state.mute})
+  }
+  subtractMedia() {
+    const {mediaPreview, mediaFiles} = this.state
+    const filterFiles = mediaFiles.filter((file => file !== mediaPreview))
+    const activeButton = document.querySelector('.activeMediaButton')
+    document.querySelector('#mediaUpload').value = ''
+    if (activeButton) {
+      activeButton.className = 'mediaThumbnail'
+    }
+    this.setState({mediaFiles: filterFiles})
+  }
+  addMedia() {
+    document.getElementById('mediaUpload').click()
   }
   getFileThumbnail(files) {
     const {mediaFiles} = this.state
@@ -63,25 +131,36 @@ class App extends React.Component {
     let key = -1
     const items = mediaFiles.map((media) => {
       key++
-      if (media.type == 'image') {
-        return <li className='mediaThumbnail' key={key}><img src={media.url}></img></li>
+      if (media.type === 'image') {
+        return <li onClick={(e) => this.mediaPreview(media, e)} className='mediaThumbnail' key={key}><img alt="Preview" src={media.url}></img></li>
       } else {
-        return <li className='mediaThumbnail' key={key}>{showVideoThumbnail(media.url)}</li>
+        return <li onClick={(e) => this.mediaPreview(media, e)} className='mediaThumbnail' key={key}>{showVideoThumbnail(media.url)}</li>
       }
     })
-    key++
-    items.push(<button className='addMediaButton' key={key} onClick={() => document.getElementById('mediaUpload').click()}>+</button>)
     return <ul className='mediaThumbnails'>{items}</ul>
   }
   render() {
-    const {activeFile, fileFormat} = this.state
-    const mainMedia = fileFormat == 'image'? showImage(activeFile): showVideo(activeFile);
+    const {activeFile, fileFormat, playing, mute} = this.state
+    const mainMedia = fileFormat === 'image'? showImage(activeFile): showVideo(activeFile, playing, mute);
     return (
       <div className="App">
         <div className='playerContainer '>
           {mainMedia}
         </div>
         <div className='mediaContainer'>
+          <div className='mediaControls'>
+            <div>
+              <button id='xButton' className='mediaButton' onClick={() => this.resetActiveFile()}></button>
+              <button id='showButton' className='mediaButton' onClick={() => this.showMedia()}></button>
+              <button id={playing? 'pauseButton': 'playButton'} className={fileFormat === 'video'? 'mediaButton': 'displayNone'} onClick={() => this.playVideo()}></button>
+              <button id={mute? 'volumeButton': 'muteButton'} className={fileFormat === 'video'? 'mediaButton': 'displayNone'} onClick={() => this.muteVideo()}></button>
+              <button id='fullscreenButton' className='mediaButton' onClick={() => this.fullScreen()}></button>
+            </div>
+            <div>
+              <button id='subtractButton' className='mediaButton' onClick={() => this.subtractMedia()}></button>
+              <button id='addButton' className='mediaButton' onClick={() => this.addMedia()}></button>
+            </div>
+          </div>
           {this.getThumbnails()}
           <input
             multiple
